501167c650490ec8cbfee377af3233995fabbc7e
Nicholas Brazeau <nbrazeau@med.unc.edu>
2018-12-27 15:50:09 -0500

    add better comments

--- data-raw/make_putative_res_sites.R
+++ data-raw/make_putative_res_sites.R
@@ -2,8 +2,10 @@
 # Previously identified loci for putative drug resistance
 devtools::load_all()
 
+#pf3d7
 pf_3d7_PutDrugRxSites <- read.table("data-raw/pf_3d7.PutDrugRxSites.bed", stringsAsFactors = FALSE)
 usethis::use_data(pf_3d7_PutDrugRxSites, overwrite = TRUE)
 
+#pvsal1
 pv_sal1_PutDrugRxSites <- read.table("data-raw/pv_sal1.PutDrugResSites.bed", stringsAsFactors = FALSE)
 usethis::use_data(pv_sal1_PutDrugRxSites, overwrite = TRUE)
