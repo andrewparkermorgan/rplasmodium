bc0801326ddf7ab99f961a8b9d58416baad9a258
Andrew Parker Morgan <andrew.parker.morgan@gmail.com>
2019-04-29 10:45:17 -0400

    update/reformat drug resistance gene lists

--- data-raw/make_putative_res_sites.R
+++ data-raw/make_putative_res_sites.R
@@ -3,13 +3,13 @@
 devtools::load_all()
 
 #pf3d7
-pf_3d7_PutDrugRxSites <- readr::read_tsv("data-raw/pf_3d7.PutDrugRxSites.bed")
+pf_3d7_PutDrugRxSites <- readr::read_tsv("data-raw/pf_3d7.drug_genes.bed", col_names = c("chr","start","end","gene_symbol","score","strand","gene_id"))
 usethis::use_data(pf_3d7_PutDrugRxSites, overwrite = TRUE)
 
 #pvsal1
-pv_sal1_PutDrugRxSites <- readr::read_tsv("data-raw/pv_sal1.PutDrugResSites.bed")
+pv_sal1_PutDrugRxSites <- readr::read_tsv("data-raw/pv_sal1.drug_genes.bed", col_names = c("chr","start","end","gene_symbol","score","strand","gene_id"))
 usethis::use_data(pv_sal1_PutDrugRxSites, overwrite = TRUE)
 
 #pvp01
-pv_p01_PutDrugRxSites <- readr::read_tsv("data-raw/pv_p01.PutDrugResSites.bed")
+pv_p01_PutDrugRxSites <- readr::read_tsv("data-raw/pv_p01.drug_genes.bed", col_names = c("chr","start","end","gene_symbol","score","strand","gene_id"))
 usethis::use_data(pv_p01_PutDrugRxSites, overwrite = TRUE)
